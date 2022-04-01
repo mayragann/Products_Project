b74ad8e0a4268230f1efb4d5c55ff092b140bce4
mayragann <mayragann@gmail.com>
2022-04-01 10:19:53 -0400

    Added the id To List To Display + Added Postman Collection

--- products/serializers.py
+++ products/serializers.py
@@ -4,5 +4,5 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['title', 'description', 'price', 'inventory_quantity']
+        fields = ['id', 'title', 'description', 'price', 'inventory_quantity']
 
